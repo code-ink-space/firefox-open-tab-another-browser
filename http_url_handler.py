6a651db1e99e68d7739b557c71c5246be2ea90ef
code-ink-space <47141674+code-ink-space@users.noreply.github.com>
2022-08-05 05:47:52 +0800

    add python script: http_url_handler.py

---
+++ http_url_handler.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python3
+import sys, os, re
+
+arg_url = sys.argv[1]
+# print(arg_url)
+
+browsers = {
+    'firefox': '/usr/bin/firefox',
+    'ffbeta': '/usr/bin/firefox-beta',
+    'gchromium': '/usr/bin/chromium',
+    'gchrome': '/usr/bin/google-chrome-stable',
+    'msedgebeta': '/usr/bin/microsoft-edge-beta',
+    'vivaldi': '/usr/bin/vivaldi-stable',
+    'librewolf': '/usr/bin/librewolf',
+}
+
+pattern_protocol = 'urlbookmarklet-protocol://'
+input_url = arg_url.replace(pattern_protocol, '')
+
+pattern_ffbeta = '_______ffbeta'
+pattern_chromium = '_______gchromium'
+pattern_chrome = '_______gchrome'
+pattern_msedgebeta = '_______msedgebeta'
+pattern_vivaldi = '_______vivaldi'
+pattern_librewolf = '_______librewolf'
+
+if input_url.endswith(pattern_ffbeta):
+    browser = browsers['ffbeta']
+    url = input_url.replace(pattern_ffbeta, '')
+elif input_url.endswith(pattern_chromium):
+    browser = browsers['gchromium']
+    url = input_url.replace(pattern_chromium, '')
+elif input_url.endswith(pattern_chrome):
+    browser = browsers['gchrome']
+    url = input_url.replace(pattern_chrome, '')
+elif input_url.endswith(pattern_msedgebeta):
+    browser = browsers['msedgebeta']
+    url = input_url.replace(pattern_msedgebeta, '')
+elif input_url.endswith(pattern_vivaldi):
+    browser = browsers['vivaldi']
+    url = input_url.replace(pattern_vivaldi, '')
+elif input_url.endswith(pattern_librewolf):
+    browser = browsers['librewolf']
+    url = input_url.replace(pattern_librewolf, '')
+else:
+    browser = browsers['firefox']
+    url = input_url
+
+cmd = browser + ' ' + url
+
+# print(cmd)
+os.system(cmd)
